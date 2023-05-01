ca980e3de0220d2e4669c835bb8cc427309243eb
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-05-01 11:49:41 -0500

    styles change

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,5 +1,5 @@
 'use strict';
-export {services};
+export { services };
 
 //Para las consultas HTTP se usa la biblioteca axios.
 const axios = require('axios/dist/browser/axios.cjs');
@@ -9,13 +9,15 @@ const API_KEY = '33886860-f2c6c3112fe87154f43438e97';
 
 //proxi
 const services = {
-  getImages: async function(searchTerm, page,RESULTS_PER_PAGE){
+  getImages: async function (searchTerm, page, RESULTS_PER_PAGE) {
     try {
-      const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${RESULTS_PER_PAGE}`);
+      const response = await axios.get(
+        `${BASE_URL}?key=${API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${RESULTS_PER_PAGE}`
+      );
       const images = response;
       return images;
     } catch (error) {
       console.error(error);
     }
-  }
-}
+  },
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
-
 'use strict';
-import '../SCSS/_common.scss'
+import '../SCSS/_common.scss';
 import '../SCSS/index.scss';
 import { services } from './axios';
+
+
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -14,25 +15,27 @@ const RESULTS_PER_PAGE = 40;
 let totalPages = 1;
 let totalImages = 1;
 let page = 1;
-let searchTerm = "asdfsaasdf";
-let markup ="";
+let searchTerm = 'asdfsaasdf';
+let markup = '';
 let lastImage;
 
-let observer = new IntersectionObserver((entrys, observer)=>{
-  console.log(entrys);
-  entrys.forEach(entry =>{
-    if(entry.isIntersecting){
-      page++;
-      console.log(page);
-      console.log(totalPages);
-      layoutUtils.refreshImagesList(searchTerm, page, RESULTS_PER_PAGE);
-    }
-  });
-},{
-  rootMargin: '0px 0px 200px 0px',
-  threshold: 1.0
-});
-
+let observer = new IntersectionObserver(
+  (entrys, observer) => {
+    console.log(entrys);
+    entrys.forEach(entry => {
+      if (entry.isIntersecting) {
+        page++;
+        console.log(page);
+        console.log(totalPages);
+        layoutUtils.refreshImagesList(searchTerm, page, RESULTS_PER_PAGE);
+      }
+    });
+  },
+  {
+    rootMargin: '0px 0px 200px 0px',
+    threshold: 1.0,
+  }
+);
 
 //Adapter
 const refs = {
@@ -40,29 +43,39 @@ const refs = {
   btnForm: document.querySelector('[type="submit"]'),
   galleryCards: document.querySelector('.gallery'),
   btnLoadMore: document.querySelector('.load-more'),
+
 };
 
 //Proxy
 const handleFormUtils = {
-  getImages: function(event){
+  getImages: function (event) {
     event.preventDefault();
-    page =1;
-    markup ="";
-    refs.galleryCards.innerHTML = "";
-    searchTerm = "asdfsaasdf";
-    if(`${refs.form['searchQuery'].value}` !== ''){
+    page = 1;
+    markup = '';
+    refs.galleryCards.innerHTML = '';
+    searchTerm = 'asdfsaasdf';
+    if (`${refs.form['searchQuery'].value}` !== '') {
       searchTerm = `${refs.form['searchQuery'].value}`;
     }
     layoutUtils.refreshImagesList(searchTerm, page, RESULTS_PER_PAGE);
-  }
-}
+  },
+};
 
 //Mediator
 const layoutUtils = {
-  renderImages: function (images){
+  renderImages: function (images) {
     markup += images
-    .map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) => {
-      return `
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `
       <div class="photo-card">
       <a class="gallery__item" href="${largeImageURL}">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" width="270" height="180" />
@@ -83,45 +96,51 @@ const layoutUtils = {
   </div>
 </div>
       `;
-    }) 
-    
-    .join("");
+        }
+      )
+
+      .join('');
     refs.galleryCards.innerHTML = markup;
     let lightbox = new SimpleLightbox('.gallery a', {
       captionsData: 'alt',
       captionDelay: 250,
-   });
-   if(page < totalPages){
-    if(lastImage){
-      observer.unobserve(lastImage);
-     }
-     const imagesOnScreen = document.querySelectorAll('.gallery .photo-card');
-     lastImage = imagesOnScreen[imagesOnScreen.length - 1];
-     observer.observe(lastImage);
+    });
+    if (page < totalPages) {
+      if (lastImage) {
+        observer.unobserve(lastImage);
+      }
+      const imagesOnScreen = document.querySelectorAll('.gallery .photo-card');
+      lastImage = imagesOnScreen[imagesOnScreen.length - 1];
+      observer.observe(lastImage);
     }
-    if(page>totalPages){
-      setTimeout(()=>{
-        Notify.info("We're sorry, but you've reached the end of search results.");
-      },DELAY);
+    if (page > totalPages) {
+      setTimeout(() => {
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }, DELAY);
     }
-   },
+  },
 
- 
-   refreshImagesList: function(searchTerm, page, RESULTS_PER_PAGE){
+  refreshImagesList: function (searchTerm, page, RESULTS_PER_PAGE) {
     services.getImages(searchTerm, page, RESULTS_PER_PAGE).then(images => {
-      if(images.data.hits.length === 0){
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+      if (images.data.hits.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
         return;
-      };
-      if(page===1){
+      }
+      if (page === 1) {
         Notify.success(`Hooray! We found ${images.data.totalHits} images.`);
         totalImages = images.data.totalHits;
         totalPages = totalImages / RESULTS_PER_PAGE;
       }
       return this.renderImages(images.data.hits);
     });
-  }
-}
+  },
+};
 
 refs.form.addEventListener('submit', handleFormUtils.getImages);
+
+
 
